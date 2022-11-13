f2f5928b5b919d77e429b3f53ecfb8a0d3f2342b
zorgensk <zorgensk@gmail.com>
2022-11-14 02:51:43 +0700

    task1fix

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -1,25 +1,3 @@
 ﻿//  Напишите программу, которая на вход принимает число (N),
 //  а на выходе показывает все чётные числа от 1 до N.
 
-Console.Write("Введите число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-if (num1 > 0)
-{
-
-for(int x = 2; x < num1; x++)
-
-{
-    if(x % 2 == 0)
-    Console.WriteLine(x);
-}
-
-}
-else 
-{
-    for(int x = 0; x > num1; x--)
-   { 
-    if(x % -2 == 0)
-    Console.WriteLine(x);
-   }
-}
